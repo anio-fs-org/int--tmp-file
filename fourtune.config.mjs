d06a2301059c90841ab9b637edd847a1ce4239ba
eXory2024 <exory@anio.software>
2024-11-09 20:10:06 +0100

    Update fourtune.config.mjs.

--- fourtune.config.mjs
+++ fourtune.config.mjs
@@ -1,18 +1,8 @@
-import {generateFromTemplate} from "fourtune/autogenerate"
-
-const asyncToSync = {
-	"import {openFile} from \"@anio-fs/api/async\"": "import {openFile} from \"@anio-fs/api/sync\"",
-	"export default async function": "export default function",
-	"await openFile(": "openFile(",
-	"await file.close()": "file.close()"
-}
-
 export default {
 	realm: "js",
-	type: "package",
+	type: "package:async/sync",
 
-	autogenerate: {
-		"sync.mjs": generateFromTemplate("src/template.mjs", asyncToSync),
-		"async.mjs": generateFromTemplate("src/template.mjs", {})
+	target: {
+		function_name: "tmpFile"
 	}
 }
